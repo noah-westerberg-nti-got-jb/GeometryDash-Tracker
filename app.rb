6b644305ec7a4f5651eb8c973cf209ed29d0705e
Noah Westerberg <noah.westerberg2@elev.ga.ntig.se>
2025-02-06 10:14:11 +0100

    Access database

--- app.rb
+++ app.rb
@@ -1,7 +1,19 @@
 class App < Sinatra::Base
 
+    def db
+        return @db if @db
+
+        @db = SQLite3::Database.new("db/GDTracker.sqlite")
+        @db.results_as_hash = true
+
+        return @db
+    end
+
     get '/' do
         erb(:index)
     end
 
+    get '/db-test' do 
+        db.execute('SELECT * FROM users').to_s
+    end
 end
