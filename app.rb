6373155dd8cebad0a95c82bd13f6b4f45cf8f180
Noah <noah.westerberg.nw@gmail.com>
2025-02-17 21:24:47 +0100

    Add: admin database interaction

--- app.rb
+++ app.rb
@@ -62,20 +62,44 @@ class App < Sinatra::Base
         end
     end
 
-    namespace '/admin', :isAdmin => '/' do
+    namespace '/admin', :isAdmin => '/login' do
         get '' do 
-            erb(:admin)
+            redirect('/admin/database')
         end
 
-        get '/view-users' do
-            def format_db_data(data) 
-                output = ""
-                data.each do |item|
-                    output += item.to_s + "<br>"
+        get '/database' do
+            erb(:database)
+        end
+
+        def format_db_data(data) 
+            output = ""
+            data.each do |item|
+                output += item.to_s + "<br>"
+            end
+            return output
+        end
+
+        get '/database-output/*' do |query_list|
+            output = ""
+
+            if query_list
+                query_list.split(';').each do |query|
+                    output += "<strong>[" + query + "]:</strong><br><br>"
+                    
+                    begin
+                        output += format_db_data(db.execute(query))
+                    rescue SQLite3::SQLException => e
+                        output += e.to_s
+                    end
+
+                    output += "<br><br>"
                 end
-                return output
             end
 
+            return output
+        end
+        
+        get '/view-users' do
             format_db_data(db.execute('SELECT * FROM users'))
         end
     end
