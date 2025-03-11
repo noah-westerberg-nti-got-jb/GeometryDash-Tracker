c8aafa95ebd6aa638d7dd8414eef96a2ec4d670b
Noah Westerberg <noah.westerberg2@elev.ga.ntig.se>
2025-03-11 09:12:10 +0100

    Update database seeder to create activity attachment table

--- db/seeder.rb
+++ db/seeder.rb
@@ -11,7 +11,8 @@ class Seeder
   def self.drop_tables
 	db.execute('DROP TABLE IF EXISTS users')
 	db.execute('DROP TABLE IF EXISTS levels')
-	db.execute('DROP TABLE IF EXISTS activity_feed')
+	db.execute('DROP TABLE IF EXISTS activity_attatchments')
+	db.execute('DROP TABLE IF EXISTS activities')
 	db.execute('DROP TABLE IF EXISTS follow_list')
 	db.execute('DROP TABLE IF EXISTS completions')
   end
@@ -27,18 +28,28 @@ class Seeder
 
     db.execute('CREATE TABLE levels (
       			ingame_id INTEGER PRIMARY KEY,
-				name TEXT NOT NULL)'
+						name TEXT NOT NULL)'
+						)
+						# TODO: Add more relevant information
+
+	db.execute('CREATE TABLE activity_attatchments (
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
+				activity_id INTEGER NOT NULL,
+				type TEXT NOT NULL,
+				link TEXT,
+				item_id INTEGER,
+				FOREIGN KEY(activity_id) REFERENCES activities(id))'
 				)
-				# TODO: Add more relevant information
 
 	db.execute('CREATE TABLE activities (
 				id INTEGER PRIMARY KEY AUTOINCREMENT,
 				user_id INTEGER NOT NULL,
-				title TEXT NOT NULL,
-				contents TEXT,
-				attatchment TEXT,
+				title TEXT NOT NULL DEFAULT "New Activity",
+				text TEXT,
+				attatchment_id INTEGER,
 				created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-				FOREIGN KEY(user_id) REFERENCES users(id))'
+				FOREIGN KEY(user_id) REFERENCES users(id),
+				FOREIGN KEY(attatchment_id) REFERENCES activity_attatchments(id))'
 				)
 
 	db.execute('CREATE TABLE follow_list (
